d6cf9c49af19dcac9a979016dacda425b9090d13
Toxix <toxix@server.fake>
2015-03-16 18:11:17 +0100

    Change to the prepend syntax and add a method to support record destroy action on globalized records

--- lib/protector/globalize.rb
+++ lib/protector/globalize.rb
@@ -2,28 +2,48 @@ require 'protector/globalize/version'
 require 'globalize'
 require 'protector'
 
-module Globalize
-  module ActiveRecord
-    class Adapter
-      def save_translations_with_protector!
-        Protector.insecurely do
-          save_translations_without_protector!
-        end
+
+module GlobalizeWithProtector
+  module Adapter
+    def save_translations!
+      Protector.insecurely do
+        super
       end
-
-      alias_method_chain :save_translations!, :protector
-    end
-
-    module InstanceMethods
-      def read_attribute_with_protector(name, options = {})
-        return nil if protector_subject? && !can?(:read, name)
-
-        Protector.insecurely do
-          read_attribute_without_protector(name, options)
-        end
-      end
-
-      alias_method_chain :read_attribute, :protector
     end
   end
+
+  module InstanceMethods
+    def read_attribute(name, options = {})
+      return nil if protector_subject? && !can?(:read, name)
+
+      Protector.insecurely do
+        super
+      end
+    end
+  end
+
+  module Base
+    def destroy
+      return super unless protector_subject?
+      return super unless self.class.method_defined?(:translations)
+
+      self.transaction do
+        Protector.insecurely do
+          self.translations.destroy_all
+        end
+        ret = super
+        if ret
+          return ret
+        else
+          raise ActiveRecord::Rollback
+          return false
+        end
+      end
+    end
+  end
+
 end
+
+Globalize::ActiveRecord::Adapter.send :prepend, GlobalizeWithProtector::Adapter
+Globalize::ActiveRecord::InstanceMethods.send :prepend, GlobalizeWithProtector::InstanceMethods
+ActiveRecord::Base.send :prepend, GlobalizeWithProtector::Base
